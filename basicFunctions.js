b05d0a5e4ae5516d26017dafcd9626b40e9bb280
Felipe <felipe@visionar.io>
2024-04-15 21:47:34 -0300

    Basic functions

---
+++ basicFunctions.js
@@ -0,0 +1,27 @@
+let r
+
+// Identity Funcion
+const I = a => a
+
+r = I(3) // 3
+r = I(I) // [Function: I]
+
+
+// Self Application Function
+const SELF = f => f(f)
+
+r = SELF(I) // [Function: I]
+
+
+// First Functino
+const FIRST = a => b => a
+
+r = FIRST(7)(11) // 7
+
+
+// Last Function
+const LAST = a => b => b
+
+r = LAST(3)(5) // 5
+
+console.log(r)
